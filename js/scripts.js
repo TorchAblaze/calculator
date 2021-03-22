7a2785778ac7af6b3b5b4b5ed95550a2c56ed17b
TorchAblaze <tiffanygreathead@gmail.com>
2021-03-22 13:31:34 -0700

    add js file with formulas

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,54 @@
-tfunction add(number1, number2) {
+function add(number1, number2) {
   return number1 + number2;
-  }
-  
-  const number1 = parseInt(prompt("Enter a number:"));
-  const number2 = parseInt(prompt("Enter another number:"));
-  
-  alert(add(number1, number2)); 
+}
+
+function subtract(number1, number2) {
+  return number1 + " - " + number2 + " = " + (number1 - number2);
+}
+
+const number3 = parseInt(prompt("Enter a number:"));
+const number4 = parseInt(prompt("Enter another number:"));
+const difference = subtract(number3, number4);
+
+alert(difference);
+
+function multiply(number3, number4) {
+  return number3 + " * " + number4 + " = " + number3 * number4;
+}
+
+const number5 = parseInt(prompt("Enter a number:"));
+const number6 = parseInt(prompt("Enter another number:"));
+const product = multiply(number5, number6);
+
+alert(product);
+
+const result = add(number1, number2);
+
+// Temp Conversion:
+
+function tempConversion(temperature) {
+  let fahTemp = (9 / 5) * temperature + 32;
+  let celTemp = (9 / 5) * temperature - 32;
+  alert(temperature + "  in fahrenheit: " + fahTemp);
+  alert(temperature + " in celsius: " + celTemp);
+}
+
+tempConversion(45);
+
+function tempConversion(temperature) {
+  let fahTemp = (9 / 5) * temperature + 32;
+  let celTemp = (9 / 5) * temperature - 32;
+  alert(temperature + "  in fahrenheit: " + fahTemp);
+  alert(temperature + " in celsius: " + celTemp);
+}
+
+tempConversion(45);
+
+function volConversion(volume) {
+  let liter = volume / 3.785;
+  let gallon = volume * 3.785;
+  alert(volume + " gallons in liters: " + liter);
+  alert(volume + " liters in gallons: " + gallon);
+}
+
+volConversion(13);
