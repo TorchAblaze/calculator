7cc4f6efb1c39ec0b0fef374e59d6d54fd0cb579
TorchAblaze <tiffanygreathead@gmail.com>
2021-03-25 00:46:29 -0700

    Add completed calculater with functioning add, subtract, multiply and division button funtions

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,35 @@
 function add(number1, number2) {
-  return number1 + " + " + number2 + " = " + (number1 + number2);
+  return number1 + number2;
 }
 
 function subtract(number1, number2) {
-  return number1 + " - " + number2 + " = " + (number1 - number2);
+  return number1 - number2;
 }
 
-const number3 = parseInt(prompt("Enter a number:"));
-const number4 = parseInt(prompt("Enter another number:"));
-const difference = subtract(number3, number4);
-
-alert(difference);
-
 function multiply(number1, number2) {
-  return number1 + " * " + number2 + " = " + number1 * number2;
+  return number1 * number2;
 }
 
 function divide(number1, number2) {
-  return number1 + " / " + number2 + " = " + number1 / number2;
+  return number1 / number2;
 }
 
-const number5 = parseInt(prompt("Enter a number:"));
-const number6 = parseInt(prompt("Enter another number:"));
-const product = multiply(number5, number6);
-
-alert(product);
-
-const result = add(number1, number2);
+$(document).ready(function () {
+  $("form#math").submit(function (event) {
+    event.preventDefault();
+    const number1 = parseInt($("#math1").val());
+    const number2 = parseInt($("#math2").val());
+    $("button#add").click(function () {
+      $("div#output").text(add(number1, number2));
+    });
+    $("button#subtract").click(function () {
+      $("div#output").text(subtract(number1, number2));
+    });
+    $("button#multiply").click(function () {
+      $("div#output").text(multiply(number1, number2));
+    });
+    $("button#divide").click(function () {
+      $("div#output").text(divide(number1, number2));
+    });
+  });
+});
